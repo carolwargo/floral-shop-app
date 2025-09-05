e75609bd146cc37d7ce7d48a1d1dc2f5b4b41a2d
Carol Wargo <carolwargo@gmail.com>
2025-09-05 16:30:20 -0400

    SERVER: Add auth with MongoDB connection, user model, and auth routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
+
 const app = express();
 
+connectDB();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('Server is running'));
 
